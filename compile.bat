d10cbd6a6375e0b5421aa02e75b8152195284953
Sean Baggaley <sean@csnxs.uk>
2018-03-30 23:07:11 +0100

    LiteSpree nearly

---
+++ compile.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set LS_VER=1a
+
+set PATH=%CD%\bin;%PATH%
+
+bcc -I "%CD%\bin\lib" -x bcs -tab-size 4 src/source/litespre.bcs src/acs/litespre.o
+if not %errorlevel%==0 (
+	pause
+	exit
+)
+
+DEL /F /Q "%CD%\build\litespree-v%LS_VER%.pk3"
+
+7za a -tzip "%CD%\build\litespree-v%LS_VER%.pk3" -r "%CD%\src\*.*" -x!.gitignore
+if not %errorlevel%==0 (
+	pause
+	exit
+)
